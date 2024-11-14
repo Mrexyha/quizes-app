ad640e73db6776cd14b58b3768fff9a21386b7ae
Mrexyha <104315683+Mrexyha@users.noreply.github.com>
2024-11-14 14:43:07 +0200

    Add animation

--- src/views/QuizesView.vue
+++ src/views/QuizesView.vue
@@ -2,6 +2,7 @@
 import q from "../data/quizes.json";
 import { ref, watch } from "vue";
 import Card from "../components/Card.vue";
+import gsap from "gsap";
 
 const quizes = ref(q);
 const search = ref("");
@@ -11,6 +12,30 @@ watch(search, () => {
     quiz.name.toLowerCase().includes(search.value.toLocaleLowerCase())
   );
 });
+const beforeEnter = (el) => {
+  // card/enter-from
+  console.log("Before enter");
+  el.style.opacity = 0;
+  el.style.transform = "translateY(-100px)";
+};
+
+const enter = (el) => {
+  // card/enter-to
+  // console.log("ENTER");
+  // el.style.opacity = 1;
+  // el.style.transform = "translateY(0)";
+  // //card-enter-active
+  gsap.to(el, {
+    y: 0,
+    opacity: 1,
+    duration: 0.3,
+    delay: el.dataset.index * 0.3,
+  });
+};
+
+const afterEnter = () => {
+  console.log("After enter");
+};
 </script>
 
 <template>
@@ -20,7 +45,20 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+      <TransitionGroup
+        appear
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+      >
+        <Card
+          v-for="(quiz, index) in quizes"
+          :key="quiz.id"
+          :quiz="quiz"
+          :data-index="index"
+        />
+      </TransitionGroup>
+
       <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="" />
         <div class="card-text">
